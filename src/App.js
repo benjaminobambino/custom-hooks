8f8faeb29c7081db994b787657e63c261a283578
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-11 19:13:13 -0800

    removed test buttonRef and added hexcode variable

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import useRandomJoke from './useRandomJoke';
 function App() {
   const firstNameRef = useRef(null);
   const lastNameRef = useRef(null);
-  const buttonRef = useRef(null);
 
   const [firstName, setFirstName] = useState('Benjamin');
   const [lastName, setLastName] = useState('Peck');
   const joke = useRandomJoke(firstName, lastName);
+
+  const bgColor = '#505050';
 
   const generateJoke = (e) => {
     e.preventDefault();
@@ -18,14 +19,14 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: bgColor }}>
       <center>
         <h1>The Joke Generator</h1>
         <h3>{joke}</h3>
         <form onSubmit={generateJoke}>
           <input placeholder="first name" ref={firstNameRef} />
           <input placeholder="last name" ref={lastNameRef} />
-          <button ref={buttonRef}>Generate Joke</button>
+          <button>Generate Joke</button>
         </form>
       </center>
     </div>
